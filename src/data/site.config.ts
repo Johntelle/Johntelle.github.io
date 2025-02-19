ae76ee7a815f5807d139584c7f8048457426c4b6
Johntelle <johntelle0407@gmail.com>
2025-02-19 18:33:11 -0300

    Update site.config.ts

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -10,8 +10,8 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
+	site: 'https://johntelle.github.io', // Write here your website url
+	author: 'Johntelle', // Site author
 	title: 'Astro Theme OpenBlog', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
 	lang: 'en-GB',
